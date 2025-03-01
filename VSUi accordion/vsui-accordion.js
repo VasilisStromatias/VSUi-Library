11867a0bc759a0162d9d2d631afe1ed2de4012b1
vstromatias <stromatiasvasilis@gmail.com>
2025-03-01 02:40:35 +0200

    animation, open property
    Add some animation to the accordion when opening, add the open property ( user selects which accordion wants to be initially opened | default value = false)

--- VSUi accordion/vsui-accordion.js
+++ VSUi accordion/vsui-accordion.js
@@ -6,6 +6,7 @@
         initContentClass: "vsui-accordion-content",
         headerElement: "h3",
         contentElement: "div",
+        opened: false,
       },
       options
     );
@@ -15,13 +16,23 @@
       var header = container.children(settings.headerElement);
       var content = container.children(settings.contentElement);
 
-      header.addClass(settings.headerClasses);
-      content.addClass(settings.contentClasses);
+      //initial classes
       header.addClass(settings.initHeaderClass);
       content.addClass(settings.initContentClass);
+      //classes that the user adds
+      header.addClass(settings.headerClasses);
+      content.addClass(settings.contentClasses);
+
+      content.hide(); //hide all contents
+
+      if (settings.opened !== false) {
+        content.eq(settings.opened).show();
+        content.eq(settings.opened).addClass("open");
+      }
 
       header.click(function () {
-        $(this).next(settings.contentElement).toggleClass("open");
+        $(this).toggleClass("active");
+        $(this).next(settings.contentElement).toggleClass("open").slideToggle();
       });
     });
   };
--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
   $("#vsui-accordion").vsuiaccordion({
-    headerElement: "h3",
     headerClasses: "extra-class-header",
-    contentElement: "div",
     contentClasses: "extra-class-content",
+    opened: 0,
   });
 });
